7d3ccd6e4e28275c00918f5e1a0646dc9a2037fe
GacekMPL2 <gacekmpl02@wp.pl>
2023-10-24 22:46:47 +0200

    clear, update 1.20.2

--- src/main/java/me/gacekmpl2/gacekskyblockmanger/commands/GSkyBlockCommand.java
+++ src/main/java/me/gacekmpl2/gacekskyblockmanger/commands/GSkyBlockCommand.java
@@ -1,15 +1,10 @@
 package me.gacekmpl2.gacekskyblockmanger.commands;
 
-import jdk.javadoc.internal.doclets.formats.html.markup.Text;
 import me.gacekmpl2.gacekskyblockmanger.essentials.ChatUtils;
 import me.gacekmpl2.gacekskyblockmanger.essentials.ConfigUtils;
-import net.kyori.adventure.text.event.ClickEvent;
-import net.kyori.adventure.text.event.HoverEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
